2b05791586daa51c69cb4efed877209318b5c722
HatimZuzzer <hatimzs1@hotmail.com>
2014-06-25 19:26:01 +0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,7 +4,7 @@ var sectionHeight = function() {
 
   if ($section.outerHeight(true) < total) {
     var margin = $section.outerHeight(true) - $section.height();
-    $section.height(total - margin - 10);
+    $section.height(total - margin - 20);
   } else {
     $section.css('height','auto');
   }
